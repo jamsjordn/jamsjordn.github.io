b8509951b06b048f25798beb5e97f3a2b2f4ee39
James <james.jordan@live.com.au>
2016-08-31 01:06:28 +1000

    s

--- about/script.js
+++ about/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){ 
   $.get("header.html", function(data) {
     $("#header").html(data);
-  });
-}); 
+  }); 
+while ($( "#about" ).hasClass( "active" ) == false){
+	$("#about").addClass("active");
+};
+});
