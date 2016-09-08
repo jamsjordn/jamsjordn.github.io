db1e0e2503d2db6fe97d15e2fe95cb7f1d13af56
James <james.jordan@live.com.au>
2016-09-08 15:01:31 +1000

    sesg

---
+++ resources/constants/foot.js
@@ -0,0 +1,8 @@
+document.write('\
+	<script>\
+		window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')\
+	</script>\
+	<script src="/resources/js/bootstrap.min.js"></script>\
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->\
+	<script src="/resources/js/ie10-viewport-bug-workaround.js"></script>\
+');
--- resources/constants/head.js
+++ resources/constants/head.js
@@ -6,11 +6,13 @@ document.write('\
     <meta name="description" content="">\
     <meta name="author" content="">\
     <!--link rel="icon" href="../../favicon.ico"-->\
-    <link href="../resources/css/bootstrap.min.css" rel="stylesheet">\
-    <link href="../resources/css/ie10-viewport-bug-workaround.css" rel="stylesheet">\
+    <link href="/resources/css/bootstrap.min.css" rel="stylesheet">\
+    <link href="/resources/css/ie10-viewport-bug-workaround.css" rel="stylesheet">\
     <link href="starter-template.css" rel="stylesheet">\
-    <link href="../resources/css/font-awesome.css" rel="stylesheet">\
+    <link href="/resources/css/font-awesome.css" rel="stylesheet">\
 	<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"> \
-    <script src="../resources/js/ie-emulation-modes-warning.js"></script>\
-    <link href="../resources/css/custom.css" rel="stylesheet">\
+    <script src="/resources/js/ie-emulation-modes-warning.js"></script>\
+    <link href="/resources/css/custom.css" rel="stylesheet">\
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>\
+	<script src="/resources/js/bootstrap.min.js"></script>\
 ');
