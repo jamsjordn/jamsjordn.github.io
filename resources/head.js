fd92ea0bf1d7312c85f64795d9b1344d90dd4684
James <james.jordan@live.com.au>
2016-10-31 21:32:14 +1100

    icon

--- resources/head.js
+++ resources/head.js
@@ -4,4 +4,5 @@ document.write('\
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>\
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">\
 	<link href="site-colours.css" rel="stylesheet">\
+	<link rel="shortcut icon" href="ico.ico" type="image/x-icon">\
 ');
