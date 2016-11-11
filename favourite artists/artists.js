13e03d8007a0c14b8b2df05e9044d48d7a78dfc8
James <james.jordan@live.com.au>
2016-11-12 04:07:00 +1100

    js

---
+++ favourite artists/artists.js
@@ -0,0 +1,42 @@
+var artists = [
+	{
+		name: 'Ellie Goulding',
+		image: '/assets/img/EllieGoulding.jpg',
+		desc: 'Top notch british stuff'
+	},
+	{
+		name: 'Bastille',
+		image: '/assets/img/Bastille.jpg',
+		desc: 'Top notch british stuff'
+	},
+	{
+		name: 'Betty Who',
+		image: '/assets/img/BettyWho.jpg',
+		desc: 'Top notch british stuff'
+	},
+	{
+		name: 'Karmin',
+		image: '/assets/img/Karmin.jpg',
+		desc: 'Top notch british stuff'
+	},
+	{
+		name: 'Foxes',
+		image: '/assets/img/Foxes.jpg',
+		desc: 'Top notch british stuff'
+	},
+	{
+		name: 'Years & Years',
+		image: '/assets/img/YearsYears.jpg',
+		desc: 'Top notch british stuff'
+	},
+	{
+		name: 'Broods',
+		image: '/assets/img/Broods.jpg',
+		desc: 'Top notch british stuff'
+	},
+	{
+		name: 'Kelly Clarkson',
+		image: '/assets/img/KellyClarkson.jpg',
+		desc: 'Top notch british stuff'
+	},
+]
