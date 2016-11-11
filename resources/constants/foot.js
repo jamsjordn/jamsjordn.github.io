8365e9a6c17ce56fea33471918169e84a68c44fe
James <james.jordan@live.com.au>
2016-11-11 17:30:23 +1100

    overhaul

--- resources/constants/foot.js
+++
@@ -1,8 +0,0 @@
-document.write('\
-	<script>\
-		window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')\
-	</script>\
-	<script src="/resources/js/bootstrap.min.js"></script>\
-	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->\
-	<script src="/resources/js/ie10-viewport-bug-workaround.js"></script>\
-');
