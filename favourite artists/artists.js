a3baf456d7bee415e125fb7387a4835900ea929b
James <james.jordan@live.com.au>
2016-11-14 21:48:18 +1100

    more pngs

--- favourite artists/artists.js
+++ favourite artists/artists.js
@@ -1,42 +1,42 @@
 var artists = [
 	{
 		name: 'Ellie Goulding',
-		image: '/assets/img/EllieGoulding.jpg',
+		image: '/assets/img/EllieGoulding.png',
 		desc: 'Top notch british stuff'
 	},
 	{
 		name: 'Bastille',
-		image: '/assets/img/Bastille.jpg',
+		image: '/assets/img/Bastille.png',
 		desc: 'Top notch british stuff'
 	},
 	{
 		name: 'Betty Who',
-		image: '/assets/img/BettyWho.jpg',
+		image: '/assets/img/BettyWho.png',
 		desc: 'Top notch british stuff'
 	},
 	{
 		name: 'Karmin',
-		image: '/assets/img/Karmin.jpg',
+		image: '/assets/img/Karmin.png',
 		desc: 'Top notch british stuff'
 	},
 	{
 		name: 'Foxes',
-		image: '/assets/img/Foxes.jpg',
+		image: '/assets/img/Foxes.png',
 		desc: 'Top notch british stuff'
 	},
 	{
 		name: 'Years & Years',
-		image: '/assets/img/YearsYears.jpg',
+		image: '/assets/img/YearsYears.png',
 		desc: 'Top notch british stuff'
 	},
 	{
 		name: 'Broods',
-		image: '/assets/img/Broods.jpg',
+		image: '/assets/img/Broods.png',
 		desc: 'Top notch british stuff'
 	},
 	{
 		name: 'Kelly Clarkson',
-		image: '/assets/img/KellyClarkson.jpg',
+		image: '/assets/img/KellyClarkson.png',
 		desc: 'Top notch british stuff'
 	},
 ]
